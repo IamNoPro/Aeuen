8d4015334b52ce25c678ea7d2ddbd88113561cc2
Mukhtar Kussaiynbekov <mukhtarkussaiynbekov@gmail.com>
2021-05-15 15:57:59 +0900

    style header

---
+++ app/src/components/Header.jsx
@@ -0,0 +1,24 @@
+import React from 'react';
+
+const Header = () => {
+	return (
+		<header className="row">
+			<span className="col-8" id="app-title">
+				Aeuen
+			</span>
+			<div className="col-4 header-events">
+				<div className="header-event">
+					<i className="far fa-compass"></i>
+					<span>Other Events</span>
+				</div>
+				<div className="header-event">
+					<i className="far fa-calendar-check"></i>
+					<span>My Events</span>
+				</div>
+			</div>
+			<hr></hr>
+		</header>
+	);
+};
+
+export default Header;
