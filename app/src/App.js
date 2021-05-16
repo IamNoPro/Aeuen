16d57d2893f5e29fbd29e071084075009c3f79c1
Pollux <a.aidyn00@gmail.com>
2021-05-16 21:43:35 +0900

    demo done

--- app/src/App.js
+++ app/src/App.js
@@ -1,24 +1,38 @@
-import logo from './logo.svg';
 import './App.css';
+import Header from "./Header";
+import OtherEvents from "./OtherEvents";
+import {
+    BrowserRouter as Router,
+    Switch,
+    Route,
+    Link
+} from "react-router-dom";
+import mapboxgl from '!mapbox-gl'; // eslint-disable-line import/no-webpack-loader-syntax
+// import 'antd/dist/antd.css';
+// import 'bootstrap/dist/css/bootstrap.min.css';
+
+mapboxgl.accessToken = 'pk.eyJ1IjoicG9sbHV4eCIsImEiOiJja29qcWEybDQxZWlqMndvOXh5bGJkMXh4In0.-F11fMMGsYF9SMiEG-PP3w';
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <Router>
+        <div className="App">
+            <div className={'container'}>
+                <Header />
+                <Switch>
+                    <Route path={'/other-events'}>
+                        <OtherEvents />
+                    </Route>
+                    <Route path={'/my-events'}>
+                        My Events
+                    </Route>
+                    <Route path={'/'}>
+                        Home
+                    </Route>
+                </Switch>
+            </div>
+        </div>
+    </Router>
   );
 }
 
