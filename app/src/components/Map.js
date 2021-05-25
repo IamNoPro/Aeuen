410879c07e2bdbfaba6de0009117051152016305
nurlykhank <nurlykhank@gmail.com>
2021-05-25 12:54:38 +0500

    img upload & datetime

--- app/src/components/Map.js
+++ app/src/components/Map.js
@@ -7,12 +7,8 @@ import PlacesAutoComplete, {
 import mapboxgl from '!mapbox-gl'; // eslint-disable-line import/no-webpack-loader-syntax
 
 
-export default function MyMap() {
-  const [address, setAddress] = React.useState('');
-  const [cooordinates, setCoordinates] = React.useState({
-    lat: null,
-    lng: null
-  });
+const MyMap = (props) => {
+  
 	const mapContainer = useRef(null);
 	const map = useRef(null);
   const zoom = 4;
@@ -31,8 +27,8 @@ export default function MyMap() {
     const results = await geocodeByAddress(value);
     const latLng = await getLatLng(results[0]);
   
-    setAddress(value);
-    setCoordinates(latLng);
+    props.setAddress(value);
+    props.setCoordinates(latLng);
 
     map.current.setCenter([latLng['lng'], latLng['lat']]);
     map.current.setZoom(15);
@@ -41,8 +37,8 @@ export default function MyMap() {
   return (
     <div>
       <PlacesAutoComplete 
-        value={address} 
-        onChange={setAddress} 
+        value={props.address} 
+        onChange={props.setAddress} 
         onSelect={handleSelect}
       >
         {({ getInputProps, suggestions, getSuggestionItemProps, loading }) => (
@@ -72,3 +68,5 @@ export default function MyMap() {
     </div>
   );
 }
+
+export default MyMap
--- app/src/firebase.js
+++ app/src/firebase.js
@@ -1,12 +1,14 @@
 import firebase from 'firebase/app';
 import 'firebase/auth';
 import 'firebase/firestore';
+import 'firebase/storage'
 
 // Your web app's Firebase configuration
 var firebaseConfig = {
 	apiKey: 'AIzaSyCtr6B_isvJrS5pqFaSq7uO5Bg6B-go7LA',
 	authDomain: 'aeuen-deb1e.firebaseapp.com',
 	projectId: 'aeuen-deb1e',
+	storageBucket: 'aeuen-deb1e.appspot.com',
 	appId: '1:249456415564:web:8f0186254d65b0d9d635e9'
 };
 
@@ -18,6 +20,7 @@ if (firebase.apps.length === 0) {
 // make auth and firestore references
 export const auth = firebase.auth();
 export const db = firebase.firestore();
+export const storage = firebase.storage();
 
 // update firestore settings
 db.settings({ timestampsInSnapshots: true });
