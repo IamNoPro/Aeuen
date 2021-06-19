c532b185aaee10f0e5848afc5f93e2d0cb15ba4e
Mukhtar Kussaiynbekov <mukhtarkussaiynbekov@gmail.com>
2021-06-19 14:46:57 +0900

    make profile photo mandatory

--- app/src/components/Signup.js
+++ app/src/components/Signup.js
@@ -1,8 +1,8 @@
 import React, { useState } from 'react';
 import { auth } from '../firebase';
-import firebase from "firebase/app";
-import { useHistory } from "react-router-dom";
-import { storage } from '../firebase'
+import firebase from 'firebase/app';
+import { useHistory } from 'react-router-dom';
+import { storage } from '../firebase';
 
 export const isValidEmail = email => {
 	// referenced https://www.simplilearn.com/tutorials/javascript-tutorial/email-validation-in-javascript
@@ -13,7 +13,7 @@ export const isValidEmail = email => {
 
 const Signup = () => {
 	let history = useHistory();
-    let users = firebase.firestore().collection('users');
+	let users = firebase.firestore().collection('users');
 
 	const [user, setUser] = useState({
 		email: '',
@@ -27,13 +27,14 @@ const Signup = () => {
 		name: '',
 		password: '',
 		passwordConfirm: '',
+		file: '',
 		message: ''
 	});
 
 	const [selectedFile, setSelectedFile] = useState({
-        name: null,
-        file: null
-    });
+		name: null,
+		file: null
+	});
 
 	const onChange = event => {
 		let input = event.target.value;
@@ -42,39 +43,38 @@ const Signup = () => {
 		setErrors({ ...errors, [property]: '', message: '' });
 	};
 
+	const addFirestore = uid => {
+		if (!selectedFile.raw_file) return;
 
-    const addFirestore = (uid) => {
-        if (!selectedFile.raw_file) return;
-		
 		// console.log(selectedFile.file);
 
-        const uploadTask = storage.ref(`profile_images/${selectedFile.name}`).put(selectedFile.raw_file);
-        uploadTask.on(
-          "state_changed",
-          snapshot => {
-          },
-          error => {
-            console.log(error)
-          },
-          () => {
-            storage
-              .ref("profile_images")
-              .child(selectedFile.name)
-              .getDownloadURL()
-              .then(url => {
-                console.log(url)
-                console.log('user uid: ' + uid)
-				
-				users.doc(uid).set({
-					'name': user.name,
-					'events': [],
-					'picture': url,
-				})
+		const uploadTask = storage
+			.ref(`profile_images/${selectedFile.name}`)
+			.put(selectedFile.raw_file);
+		uploadTask.on(
+			'state_changed',
+			snapshot => {},
+			error => {
+				console.log(error);
+			},
+			() => {
+				storage
+					.ref('profile_images')
+					.child(selectedFile.name)
+					.getDownloadURL()
+					.then(url => {
+						console.log(url);
+						console.log('user uid: ' + uid);
 
-              })
-          }
-        )
-		}
+						users.doc(uid).set({
+							name: user.name,
+							events: [],
+							picture: url
+						});
+					});
+			}
+		);
+	};
 
 	const onSubmit = () => {
 		let validInputs = true;
@@ -99,6 +99,10 @@ const Signup = () => {
 			currentErrors.name = 'Full Name cannot be empty';
 			validInputs = false;
 		}
+		if (!selectedFile.name) {
+			currentErrors.file = 'Profile photo is needed';
+			validInputs = false;
+		}
 		if (!validInputs) {
 			setErrors(currentErrors);
 			return;
@@ -106,127 +110,136 @@ const Signup = () => {
 
 		auth
 			.createUserWithEmailAndPassword(user.email, user.password)
-			.then(function(data) {
+			.then(function (data) {
 				console.log(data.user.uid);
-				
+
 				addFirestore(data.user.uid);
 
 				users.doc(data.user.uid).set({
-					'name': user.name,
-					'events': []
-				})
+					name: user.name,
+					events: []
+				});
 
-				history.push({pathname: "/my-events", state: {user_uid: data.user.uid}});
+				history.push({
+					pathname: '/my-events',
+					state: { user_uid: data.user.uid }
+				});
 			})
 			.catch(error => setErrors({ ...errors, message: error }));
 	};
 
-	const onChangeFile = (event) => {
-        console.log('selected file', event.target.files[0]);
-        setSelectedFile({
-                        name: event.target.files[0].name,
-                        file: URL.createObjectURL(event.target.files[0]),
-						raw_file: event.target.files[0]
-                    });
-    }
+	const onChangeFile = event => {
+		console.log('selected file', event.target.files[0]);
+		setSelectedFile({
+			name: event.target.files[0].name,
+			file: URL.createObjectURL(event.target.files[0]),
+			raw_file: event.target.files[0]
+		});
+	};
 
 	return (
 		<div className="content">
 			<div className="left-content" />
-				<div className="mid-content container-create">
-					<form className="create-event">
-						<div className="form-control form-control-login" >
-							<h1 align="center">Sign Up</h1>
-							{errors.message && (
-								<div style={{ color: 'red' }}>{errors.message}</div>
-							)}
-						</div>
-						
-						<div className="form-control form-control-login" >
-							<label>Email</label>
+			<div className="mid-content container-create">
+				<form className="create-event">
+					<div className="form-control form-control-login">
+						<h1 align="center">Sign Up</h1>
+						{errors.message && (
+							<div style={{ color: 'red' }}>{errors.message}</div>
+						)}
+					</div>
+
+					<div className="form-control form-control-login">
+						<label>Email</label>
+						<input
+							name="email"
+							type="text"
+							placeholder="email@example.com"
+							value={user.email}
+							onChange={onChange}
+						/>
+						{errors.email && <div style={{ color: 'red' }}>{errors.email}</div>}
+					</div>
+
+					<div className="form-control form-control-login">
+						<label>Full Name</label>
+						<input
+							name="name"
+							type="text"
+							placeholder="Elon Musk"
+							value={user.name}
+							onChange={onChange}
+						/>
+						{errors.name && <div style={{ color: 'red' }}>{errors.name}</div>}
+					</div>
+
+					<div className="form-control form-control-login">
+						<label>Password</label>
+						<input
+							name="password"
+							type="password"
+							placeholder="Enter password"
+							value={user.password}
+							onChange={onChange}
+						/>
+						{errors.password && (
+							<div style={{ color: 'red' }}>{errors.password}</div>
+						)}
+					</div>
+
+					<div className="form-control form-control-login">
+						<label>Confirm Password</label>
+						<input
+							name="passwordConfirm"
+							type="password"
+							placeholder="Confirm password"
+							value={user.passwordConfirm}
+							onChange={onChange}
+						/>
+						{errors.passwordConfirm && (
+							<div style={{ color: 'red' }}>{errors.passwordConfirm}</div>
+						)}
+					</div>
+
+					<div className="form-control form-control-login">
+						<label>Profile Picture</label>
+						<label
+							className="form-control-poster"
+							style={{ width: selectedFile.file ? '275px' : '150px' }}
+						>
+							{selectedFile.file ? selectedFile.name : 'Upload Picture'}{' '}
+							<i className="fa fa-upload"></i>
 							<input
-								name="email"
-								type="text"
-								placeholder="email@example.com"
-								value={user.email}
-								onChange={onChange}
+								id="upload"
+								type="file"
+								accept="image/*"
+								onChange={onChangeFile}
 							/>
-							{errors.email && (
-								<div style={{ color: 'red' }}>{errors.email}</div>
-							)}
-						</div>
+						</label>
+						{errors.file && <div style={{ color: 'red' }}>{errors.file}</div>}
+					</div>
 
-						<div className="form-control form-control-login">
-							<label>Full Name</label>
-							<input
-								name="name"
-								type="text"
-								placeholder="Elon Musk"
-								value={user.name}
-								onChange={onChange}
-							/>
-							{errors.name && <div style={{ color: 'red' }}>{errors.name}</div>}
-						</div>
-
-						<div className="form-control form-control-login">
-							<label>Password</label>
-							<input
-								name="password"
-								type="password"
-								placeholder="Enter password"
-								value={user.password}
-								onChange={onChange}
-							/>
-							{errors.password && (
-								<div style={{ color: 'red' }}>{errors.password}</div>
-							)}
-						</div>
-
-						<div className="form-control form-control-login">
-							<label>Confirm Password</label>
-							<input
-								name="passwordConfirm"
-								type="password"
-								placeholder="Confirm password"
-								value={user.passwordConfirm}
-								onChange={onChange}
-							/>
-							{errors.passwordConfirm && (
-								<div style={{ color: 'red' }}>{errors.passwordConfirm}</div>
-							)}
-						</div>
-
-						<div className='form-control form-control-login'>
-                            <label>
-                                Profile Picture
-                            </label>
-                            <label className='form-control-poster' style={{width: selectedFile.file ? '275px' : '150px'}}>
-                                { selectedFile.file ? selectedFile.name : 'Upload Picture'} <i className="fa fa-upload"></i> 
-                                <input 
-                                    id="upload" 
-                                    type="file"
-                                    accept="image/*"
-                                    onChange={onChangeFile}
-                                />
-                            </label>
-                        </div>
-
-						<div style={{ marginLeft: '180px' }}>
-							<button
-								type="button"
-								value="Sign Up"
-								className="my-button"
-								onClick={onSubmit}
-							>
-								<div>Sign Up</div>
-							</button>
-						</div>
-					</form>
-				</div>
-				<div className='right-content' style={{ marginTop: '100px', marginLeft: '50px' }}>
-                	{ selectedFile.file ? <img className='poster' src={selectedFile.file} width={300}></img> : null }
-            	</div>
+					<div style={{ marginLeft: '180px' }}>
+						<button
+							type="button"
+							value="Sign Up"
+							className="my-button"
+							onClick={onSubmit}
+						>
+							<div>Sign Up</div>
+						</button>
+					</div>
+				</form>
 			</div>
+			<div
+				className="right-content"
+				style={{ marginTop: '100px', marginLeft: '50px' }}
+			>
+				{selectedFile.file ? (
+					<img className="poster" src={selectedFile.file} width={300}></img>
+				) : null}
+			</div>
+		</div>
 	);
 };
 
